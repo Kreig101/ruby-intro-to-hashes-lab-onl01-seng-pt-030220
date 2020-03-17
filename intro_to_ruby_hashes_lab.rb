170b817ba6f214f889b4223cc9d047b291c3a61d
Craig Storer <craigstorer@gmail.com>
2020-03-17 03:49:48 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -57,10 +57,18 @@ def monopoly_with_fourth_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-  monopoly = {:railroads => {:pieces => 4, :rent_in_dollars=> {:one_piece_owned=>25,
+  monopoly =  {
+  :railroads=>
+  {
+    :pieces=>4,
+    :rent_in_dollars=>
+    {
+      :one_piece_owned=>25,
       :two_pieces_owned=>50,
       :three_pieces_owned=>100,
-      :four_pieces_owned=>200},:names=>
+      :four_pieces_owned=>200
+    },
+    :names=>
     {
       :reading_railroad=>
       {
@@ -74,11 +82,13 @@ def monopoly_with_fourth_tier
       {
         "mortgage_value"=>"$400"
       },
-      :shortline =>
+      :shortline=>
       {
         "mortgage_value"=>"$800"
       }
     }
   }
 }
+
+
 end
